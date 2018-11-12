e5b08044465de7c19bd0528abb57dec197916eac
Marya <marya_apanasevich@mail.ru>
2018-11-12 10:28:50 +0300

    Added WebDriver lab

---
+++ WebDriver/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestApp
+{
+    [TestClass]
+    public class ThreadSleep
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.aircanada.com/");
+            Thread.Sleep(2000);
+            WebElement element = driver.FindElement(By.XPath("//input[@type='text'])[2]"));
+            element.SendKeys("Minsk");
+
+
+            Thread.Sleep(2000);
+
+            WebElement button_Find = driver.FindElement(By.Id("executeSingleCitySubmit_i"));
+            button_Find.Click();
+            String title = driver.Title;
+            Assert.AreEqual(true, title.Contains(expectedTitle), "The information is incomplete. Please, check it again and send the correct form");
+        }
+    }
+}
